e3ab94cf15f8d83466c4eb59082e9a603038957b
Singam Ravi Babu <115613718+singamravibabu@users.noreply.github.com>
2025-02-06 20:55:48 +0530

    Update script_env.py

--- script_env.py
+++ script_env.py
@@ -1,1 +1,13 @@
+import os
 
+# Reading environment variables
+global_message = os.getenv("GLOBAL_MESSAGE", "Default Global Message")
+pi_value = os.getenv("PI_VALUE", "Default PI Vaue")
+secret_key = os.getenv("SECRET_KEY", "No secret key found")
+runtime_message = os.getenv("RUNTIME_MESSAGE", "Default Runtime Message")
+
+# Printing environment variables
+print(f"Global message: {global_message}")
+print(f"PI Value: {pi_value}")
+print(f"Secret Key: {secret_key}")
+print(f"Runtime Message: {runtime_message}")
