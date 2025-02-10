5e2c6bcbd1b734416abe1b3cf0a93583c45adb22
Singam Ravi Babu <115613718+singamravibabu@users.noreply.github.com>
2025-02-10 20:45:13 +0530

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -3,4 +3,4 @@ with open("report.txt", "w") as file:
   file.write("GitHub Actions Artifact Example\n")
   file.write("Report generated successfully!\n")
   file.write("Timestamp: 2025-02-10\n")
-prnt("Report has been generated as 'report.txt'")
+print("Report has been generated as 'report.txt'")
